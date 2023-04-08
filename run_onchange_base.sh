e495d16dfa21dacf00a5c5c838576a5968827695
Roxas <upget_inboards@aleeas.com>
2023-04-08 23:08:12 +0200

    split setup

--- run_onchange_base.sh
+++ run_onchange_base.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-BASE="$(dirname "$0")"
+# Add nerd fonts
+required_fonts=(FiraCode FiraMono SourceCodePro JetBrainsMono)
+mkdir -p $HOME/.local/share/fonts
 
-$BASE/fedora_setup.sh
-$BASE/install_tools.sh
-$BASE/install_fonts.sh
+missing_fonts=()
+for font in ${required_fonts[@]}; do
+	if [[ ! -e "$HOME/.local/share/fonts/$font" ]]; then
+		missing_fonts+=($font)
+	fi
+done
+
+if [[ ${#missing_fonts[@]} -gt 0 ]]; then
+	git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts $HOME/nerd-fonts
+	for font in ${missing_fonts[@]}; do
+		git -C $HOME/nerd-fonts sparse-checkout add patched-fonts/$font
+		mv $HOME/nerd-fonts/patched-fonts/$font ~/.local/share/fonts/
+	done
+	rm -rf $HOME/nerd-fonts
+fi
