c5d2cd8da4beb05bfc999e777c3adfbb0f105148
Roxas <upget_inboards@aleeas.com>
2023-05-18 15:20:11 +0200

    Update .config/nvim/lua/user/plugins.lua Update .zshrc Update .zshenv

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -176,9 +176,6 @@ return {
 	{
 		"altermo/ultimate-autopair.nvim",
 		event = { "InsertEnter", "CmdlineEnter" },
-		config = function()
-			require("ultimate-autopair").setup {}
-		end,
 	},
 	{
 		"altermo/npairs-integrate-upair",
