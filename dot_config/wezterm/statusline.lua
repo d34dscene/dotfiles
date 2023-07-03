c37012851e5b10f0f593a7d98b26caf17f1b630e
Roxas <upget_inboards@aleeas.com>
2023-07-03 20:23:34 +0200

    Update .config/wezterm/statusline.lua

--- dot_config/wezterm/statusline.lua
+++ dot_config/wezterm/statusline.lua
@@ -25,7 +25,6 @@ function module.setup_statusline()
 	local music = "󰎈 "
 	local hostname = " " .. wezterm.hostname() .. " 󰊠 "
 
-
 	wezterm.on("update-status", function(window, _)
 		local metadata = io.popen "playerctl metadata -i chromium --format '{{ artist }} - {{ title }}'"
 		music = "󰎈 " .. metadata:read("*a"):gsub("[\n\r]", "")
@@ -33,7 +32,7 @@ function module.setup_statusline()
 
 		local date = wezterm.strftime " %H:%M | %A | %B %d "
 
-		window:set_right_status(bubble(music, "#89b4fa") .. bubble(date, "#f2cdcd") .. bubble(hostname, "#f38ba8") )
+		window:set_right_status(bubble(music, "#89b4fa") .. bubble(date, "#f2cdcd") .. bubble(hostname, "#f38ba8"))
 	end)
 end
 
