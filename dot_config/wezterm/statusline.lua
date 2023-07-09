a0a6800efea0d08c0c317fb31165250d7aea1a43
Roxas <upget_inboards@aleeas.com>
2023-07-09 15:37:28 +0200

    Update .config/wezterm/statusline.lua

--- dot_config/wezterm/statusline.lua
+++ dot_config/wezterm/statusline.lua
@@ -14,16 +14,34 @@ local function bubble(text, color)
 		{ Foreground = { Color = color } },
 		{ Background = { Color = "#11111b" } },
 		{ Text = "" },
-	} .. wezterm.format {
+	} .. wezterm.forma {
 		{ Foreground = { Color = "#11111b" } },
 		{ Background = { Color = "#11111b" } },
 		{ Text = " " },
 	}
 end
 
+local function slant(text, color)
+	return wezterm.format {
+		{ Foreground = { Color = color } },
+		{ Background = { Color = "#11111b" } },
+		{ Text = "" },
+		--{ Text = "" },
+	} .. wezterm.format {
+		{ Foreground = { Color = "#11111b" } },
+		{ Background = { Color = color } },
+		{ Text = text },
+	} .. wezterm.format {
+		{ Foreground = { Color = color } },
+		{ Background = { Color = "#11111b" } },
+		{ Text = "" },
+		--{ Text = "" },
+	}
+end
+
 function module.setup_statusline()
 	local music = "󰎈 "
-	local hostname = " " .. wezterm.hostname() .. " 󰊠 "
+	local hostname = " " .. wezterm.hostname() .. "  "
 
 	wezterm.on("update-status", function(window, _)
 		local metadata = io.popen "playerctl metadata -i chromium --format '{{ artist }} - {{ title }}'"
@@ -32,7 +50,8 @@ function module.setup_statusline()
 
 		local date = wezterm.strftime " %H:%M | %A | %B %d "
 
-		window:set_right_status(bubble(music, "#89b4fa") .. bubble(date, "#f2cdcd") .. bubble(hostname, "#f38ba8"))
+		--window:set_right_status(bubble(music, "#89b4fa") .. bubble(date, "#f2cdcd") .. bubble(hostname, "#f38ba8"))
+		window:set_right_status(slant(music, "#89b4fa") .. slant(date, "#f2cdcd") .. slant(hostname, "#f38ba8"))
 	end)
 end
 
