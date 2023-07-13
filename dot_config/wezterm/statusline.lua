87f531c459f085605a677e9474cfdd7f0de00d46
Roxas <upget_inboards@aleeas.com>
2023-07-13 20:56:36 +0200

    Add .config/wezterm/keybindings.lua Add .config/wezterm/statusline.lua Add .config/wezterm/wezterm.lua

---
+++ dot_config/wezterm/statusline.lua
@@ -0,0 +1,58 @@
+local wezterm = require "wezterm"
+local module = {}
+
+local function bubble(text, color)
+	return wezterm.format {
+		{ Foreground = { Color = color } },
+		{ Background = { Color = "#11111b" } },
+		{ Text = "" },
+	} .. wezterm.format {
+		{ Foreground = { Color = "#11111b" } },
+		{ Background = { Color = color } },
+		{ Text = text },
+	} .. wezterm.format {
+		{ Foreground = { Color = color } },
+		{ Background = { Color = "#11111b" } },
+		{ Text = "" },
+	} .. wezterm.forma {
+		{ Foreground = { Color = "#11111b" } },
+		{ Background = { Color = "#11111b" } },
+		{ Text = " " },
+	}
+end
+
+local function slant(text, color)
+	return wezterm.format {
+		{ Foreground = { Color = color } },
+		{ Background = { Color = "#11111b" } },
+		{ Text = "" },
+		--{ Text = "" },
+	} .. wezterm.format {
+		{ Foreground = { Color = "#11111b" } },
+		{ Background = { Color = color } },
+		{ Text = text },
+	} .. wezterm.format {
+		{ Foreground = { Color = color } },
+		{ Background = { Color = "#11111b" } },
+		{ Text = "" },
+		--{ Text = "" },
+	}
+end
+
+function module.setup_statusline()
+	local music = "󰎈 "
+	local hostname = " " .. wezterm.hostname() .. "  "
+
+	wezterm.on("update-status", function(window, _)
+		local metadata = io.popen "playerctl metadata -i chromium --format '{{ artist }} - {{ title }}'"
+		music = "󰎈 " .. metadata:read("*a"):gsub("[\n\r]", "")
+		metadata:close()
+
+		local date = wezterm.strftime " %H:%M | %A | %B %d "
+
+		--window:set_right_status(bubble(music, "#89b4fa") .. bubble(date, "#f2cdcd") .. bubble(hostname, "#f38ba8"))
+		window:set_right_status(slant(music, "#89b4fa") .. slant(date, "#f2cdcd") .. slant(hostname, "#f38ba8"))
+	end)
+end
+
+return module
---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,46 @@
+local wezterm = require "wezterm"
+local keybindings = require "keybindings"
+local statusline = require "statusline"
+local config = {}
+
+-- Provide clearer error messages ---------------------------------------------
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.launch_menu = {
+	{
+		label = "Sentinel",
+		domain = { DomainName = "SSH:jk" },
+	},
+	{
+		label = "Router",
+		domain = { DomainName = "SSH:ub" },
+	},
+	{
+		label = "Arch",
+		args = { "zsh", "-c", "~/.local/bin/distrobox-enter arch" },
+	},
+	{
+		label = "Ubuntu",
+		args = { "zsh", "-c", "~/.local/bin/distrobox-enter ubuntu" },
+	},
+}
+
+-- Config ---------------------------------------------------------------------
+config.color_scheme = "Catppuccin Mocha"
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.initial_cols = 150
+config.initial_rows = 32
+config.window_background_opacity = 0.90
+config.window_decorations = "NONE"
+config.scrollback_lines = 10000
+config.check_for_updates = false
+config.status_update_interval = 5000
+config.enable_wayland = false
+
+keybindings.apply_to_config(config)
+statusline.setup_statusline()
+
+return config
