680fc10a534a050bb6f059905752eda894272172
Roxas <upget_inboards@aleeas.com>
2023-07-15 23:04:43 +0200

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,35 +1,13 @@
 local wezterm = require "wezterm"
 local keybindings = require "keybindings"
 local statusline = require "statusline"
+local domains = require "domains"
 local config = {}
 
 -- Provide clearer error messages ---------------------------------------------
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
-
-config.launch_menu = {
-	{
-		label = "Sentinel",
-		domain = { DomainName = "SSH:jk" },
-	},
-	{
-		label = "Router",
-		domain = { DomainName = "SSH:ub" },
-	},
-	{
-		label = "Fedora",
-		args = { "zsh", "-c", "~/.local/bin/distrobox-enter fedora" },
-	},
-	{
-		label = "Ubuntu",
-		args = { "zsh", "-c", "~/.local/bin/distrobox-enter ubuntu" },
-	},
-	{
-		label = "Arch",
-		args = { "zsh", "-c", "~/.local/bin/distrobox-enter arch" },
-	},
-}
 
 -- Config ---------------------------------------------------------------------
 config.color_scheme = "Catppuccin Mocha"
@@ -38,11 +16,12 @@ config.tab_bar_at_bottom = true
 config.initial_cols = 150
 config.initial_rows = 32
 config.window_background_opacity = 0.90
-config.window_decorations = "RESIZE"
+config.window_decorations = "NONE"
 config.scrollback_lines = 10000
 config.check_for_updates = false
 config.status_update_interval = 5000
 
+domains.apply_to_config(config)
 keybindings.apply_to_config(config)
 statusline.setup_statusline()
 
