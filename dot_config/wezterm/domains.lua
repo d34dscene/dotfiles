cfcfe8c0bc8ecbeab46d1fbb410953befed46cc2
Roxas <upget_inboards@aleeas.com>
2023-07-16 00:05:46 +0200

    Update .config/wezterm/domains.lua Update .zshrc

--- dot_config/wezterm/domains.lua
+++ dot_config/wezterm/domains.lua
@@ -1,4 +1,35 @@
+local wezterm = require "wezterm"
 local module = {}
+
+local function distro_list()
+	local boxes = io.popen "podman ps -a --format '{{.Names}}'"
+	local distros = {}
+	for line in boxes:lines() do
+		table.insert(distros, line)
+	end
+	return distros
+end
+
+local exec_domains = {}
+local exec_entries = {}
+for _, name in pairs(distro_list()) do
+	table.insert(
+		exec_domains,
+		wezterm.exec_domain("DIST:" .. name, function(cmd)
+			local wrapped = {
+				"bash",
+				"-c",
+				"/usr/bin/distrobox-enter " .. name,
+			}
+			cmd.args = wrapped
+			return cmd
+		end)
+	)
+	table.insert(exec_entries, {
+		label = name:gsub("^%l", string.upper),
+		domain = { DomainName = "DIST:" .. name },
+	})
+end
 
 function module.apply_to_config(config)
 	config.launch_menu = {
@@ -11,18 +42,19 @@ function module.apply_to_config(config)
 			domain = { DomainName = "SSH:ub" },
 		},
 		{
-			label = "DIST:Fedora",
-			args = { "bash", "-c", "/usr/bin/distrobox-enter fedora" },
+			label = "Fedora",
+			domain = { DomainName = "DIST:fedora" },
 		},
 		{
-			label = "DIST:Ubuntu",
-			args = { "bash", "-c", "/usr/bin/distrobox-enter ubuntu" },
+			label = "Ubuntu",
+			domain = { DomainName = "DIST:ubuntu" },
 		},
 		{
-			label = "DIST:Arch",
-			args = { "bash", "-c", "/usr/bin/distrobox-enter arch" },
+			label = "Arch",
+			domain = { DomainName = "DIST:arch" },
 		},
 	}
+	config.exec_domains = exec_domains
 end
 
 return module
