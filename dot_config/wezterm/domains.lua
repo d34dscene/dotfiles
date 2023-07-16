54e3136caff246844238ece948500d80b7fa661c
Roxas <upget_inboards@aleeas.com>
2023-07-16 16:59:40 +0200

    Update .config/wezterm/domains.lua Update .config/wezterm/statusline.lua Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/domains.lua
+++ dot_config/wezterm/domains.lua
@@ -2,59 +2,65 @@ local wezterm = require "wezterm"
 local module = {}
 
 local function distro_list()
-	local boxes = io.popen "podman ps -a --format '{{.Names}}'"
+	local handle = io.popen "podman ps -a --format '{{.Names}}'"
 	local distros = {}
-	for line in boxes:lines() do
-		table.insert(distros, line)
+	if handle then
+		for line in handle:lines() do
+			table.insert(distros, line)
+		end
+		handle:close()
 	end
 	return distros
 end
 
-local exec_domains = {}
-local exec_entries = {}
-for _, name in pairs(distro_list()) do
-	table.insert(
-		exec_domains,
-		wezterm.exec_domain("DIST:" .. name, function(cmd)
-			local wrapped = {
-				"bash",
-				"-c",
-				"/usr/bin/distrobox-enter " .. name,
-			}
-			cmd.args = wrapped
-			return cmd
-		end)
-	)
-	table.insert(exec_entries, {
-		label = name:gsub("^%l", string.upper),
-		domain = { DomainName = "DIST:" .. name },
-	})
+local function get_exec_domains()
+	local domains = {}
+	for _, name in pairs(distro_list()) do
+		table.insert(
+			domains,
+			wezterm.exec_domain("DIST@" .. name, function(cmd)
+				local wrapped = {
+					"bash",
+					"-c",
+					"/usr/bin/distrobox-enter " .. name,
+				}
+				cmd.args = wrapped
+				return cmd
+			end)
+		)
+	end
+	return domains
+end
+
+local function get_exec_entries()
+	local entries = {}
+	for _, name in pairs(distro_list()) do
+		table.insert(entries, {
+			label = name:gsub("^%l", string.upper),
+			domain = { DomainName = "DIST@" .. name },
+		})
+	end
+	return entries
+end
+
+local base_domains = {
+	{
+		label = "Sentinel",
+		domain = { DomainName = "SSH:jk" },
+	},
+	{
+		label = "Router",
+		domain = { DomainName = "SSH:ub" },
+	},
+}
+
+for _, v in pairs(get_exec_entries()) do
+	table.insert(base_domains, v)
 end
 
 function module.apply_to_config(config)
-	config.launch_menu = {
-		{
-			label = "Sentinel",
-			domain = { DomainName = "SSH:jk" },
-		},
-		{
-			label = "Router",
-			domain = { DomainName = "SSH:ub" },
-		},
-		{
-			label = "Fedora",
-			domain = { DomainName = "DIST:fedora" },
-		},
-		{
-			label = "Ubuntu",
-			domain = { DomainName = "DIST:ubuntu" },
-		},
-		{
-			label = "Arch",
-			domain = { DomainName = "DIST:arch" },
-		},
-	}
-	config.exec_domains = exec_domains
+	config.launch_menu = base_domains
+	config.exec_domains = get_exec_domains()
 end
 
 return module
--- dot_config/wezterm/statusline.lua
+++ dot_config/wezterm/statusline.lua
@@ -41,9 +41,10 @@ end
 
 function module.setup_statusline()
 	local music = "󰎈 "
-	local hostname = " " .. wezterm.hostname() .. "  "
 
 	wezterm.on("update-status", function(window, _)
+		local current_domain = wezterm.mux.get_domain(nil):name()
+		local hostname = " " .. wezterm.hostname() .. "@" .. current_domain .. "  "
 		-- local metadata = io.popen "playerctl metadata -i chromium --format '{{ artist }} - {{ title }}'"
 		-- music = "󰎈 " .. metadata:read("*a"):gsub("[\n\r]", "")
 		-- metadata:close()
@@ -51,7 +52,7 @@ function module.setup_statusline()
 		local date = wezterm.strftime " %H:%M | %A | %B %d "
 
 		--window:set_right_status(bubble(music, "#89b4fa") .. bubble(date, "#f2cdcd") .. bubble(hostname, "#f38ba8"))
-		window:set_right_status(slant(date, "#f2cdcd") .. slant(hostname, "#f38ba8"))
+		window:set_right_status(slant(date, "#cba6f7") .. slant(hostname, "#f38ba8"))
 	end)
 end
 
