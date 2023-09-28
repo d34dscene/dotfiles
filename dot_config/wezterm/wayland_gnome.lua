394fc377c0c9e8854e3ad4ad1cae1a22c22d03dc
d34dsc3n3 <upget_inboards@aleeas.com>
2023-09-28 13:04:03 +0200

    Add .config/wezterm/wayland_gnome.lua

---
+++ dot_config/wezterm/wayland_gnome.lua
@@ -0,0 +1,30 @@
+local wezterm = require "wezterm"
+local mod = {}
+
+local function gsettings(key)
+	return wezterm.run_child_process { "gsettings", "get", "org.gnome.desktop.interface", key }
+end
+
+function mod.apply_to_config(config)
+	if wezterm.target_triple ~= "x86_64-unknown-linux-gnu" then
+		-- skip if not running on linux
+		return
+	end
+	local success, stdout, _ = gsettings "cursor-theme"
+	if success then
+		config.xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
+	end
+
+	success, stdout, _ = gsettings "cursor-size"
+	if success then
+		config.xcursor_size = tonumber(stdout)
+	end
+
+	config.enable_wayland = true
+
+	if config.enable_wayland and os.getenv "WAYLAND_DISPLAY" then
+		success, stdout, _ = gsettings "text-scaling-factor"
+	end
+end
+
+return mod
