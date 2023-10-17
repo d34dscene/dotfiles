5e6a681053ba33fd4515deda0868a8fd0ace65a3
d34dsc3n3 <upget_inboards@aleeas.com>
2023-10-17 11:09:53 +0200

    Update tools.sh

--- run_onchange_tools.sh
+++ run_onchange_tools.sh
@@ -10,30 +10,29 @@ check() {
 fetch() {
 	URL="https://api.github.com/repos/${1}/releases/latest"
 	MATCH="[.assets[] | select(.name | contains(\"${2}\")).browser_download_url][0]"
-	LATEST=$(wget -qO- ${URL} | jq -r "${MATCH}")
+	LATEST=$(wget -qO- "${URL}" | jq -r "${MATCH}")
 	EXTENSION="${LATEST##*.}"
 
 	DLPATH="${HOME}/.local/bin"
 	if [[ "${EXTENSION}" == "gz" ]]; then
-		wget -O ${DLPATH}/dl.tar.gz ${LATEST}
-		tar -xzf ${DLPATH}/dl.tar.gz -C ${DLPATH}
-		rm ${DLPATH}/dl.tar.gz
+		wget -O "${DLPATH}"/dl.tar.gz "${LATEST}"
+		tar -xzf "${DLPATH}"/dl.tar.gz -C "${DLPATH}"
+		rm "${DLPATH}"/dl.tar.gz
 	elif [[ "${EXTENSION}" == "zip" ]]; then
-		wget -O ${DLPATH}/dl.zip ${LATEST}
-		unzip ${DLPATH}/dl.zip -d ${DLPATH}
-		rm ${DLPATH}/dl.zip
+		wget -O "${DLPATH}"/dl.zip "${LATEST}"
+		unzip "${DLPATH}"/dl.zip -d "${DLPATH}"
+		rm "${DLPATH}"/dl.zip
 	else
-		wget -O ${DLPATH}/${3} ${LATEST}
+		wget -O "${DLPATH}"/"${3}" "${LATEST}"
 	fi
 
 	# Cleanup
-	chmod +x ${DLPATH}/${3}
+	chmod +x "${DLPATH}"/"${3}"
 	rm -R -- */
-	rm ${DLPATH}/*.md ${DLPATH}/LICENSE
+	rm "${DLPATH}"/*.md "${DLPATH}"/LICENSE
 }
 
 type wormhole &>/dev/null || fetch "magic-wormhole/magic-wormhole.rs" "wormhole-rs" "wormhole"
-type doggo &>/dev/null || fetch "mr-karan/doggo" "linux_amd64" "doggo"
 
 py_tools=(
 	black
@@ -63,19 +62,19 @@ rust_tools=(
 
 if check "pip"; then
 	for tool in ${py_tools[@]}; do
-		pip install $tool
+		pip install "$tool"
 	done
 fi
 
 if check "npm"; then
 	for tool in ${node_tools[@]}; do
-		sudo npm install -g $tool
+		sudo npm install -g "$tool"
 	done
 fi
 
 if check "cargo"; then
 	for tool in ${rust_tools[@]}; do
-		cargo install $tool
+		cargo install "$tool"
 	done
 fi
 
@@ -87,13 +86,14 @@ if check "go"; then
 	check "goimports" || go install golang.org/x/tools/cmd/goimports@latest
 	check "gomodifytags" || go install github.com/fatih/gomodifytags@latest
 	check "flarectl" || go install github.com/cloudflare/cloudflare-go/cmd/flarectl@latest
+	check "doggo" || go install github.com/mr-karan/doggo/cmd/doggo@latest
 	check "k9s" || go install github.com/derailed/k9s@latest
 	check "duf" || go install github.com/muesli/duf@latest
 fi
 
 # Add zsh plugin manager
 if [[ -e "$HOME/.config/antidote" ]]; then
-	git -C $HOME/.config/antidote pull
+	git -C "$HOME"/.config/antidote pull
 else
-	git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.config/antidote
+	git clone --depth=1 https://github.com/mattmc3/antidote.git "${ZDOTDIR:-~}"/.config/antidote
 fi
