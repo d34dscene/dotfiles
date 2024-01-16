868b97b99b5e9eab14467e081b473953e75eaffc
d34dsc3n3 <upget_inboards@aleeas.com>
2024-01-16 02:42:46 +0100

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -13,15 +13,12 @@ end
 config.color_scheme = "Catppuccin Mocha"
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
---config.initial_cols = 150
---config.initial_rows = 32
 config.window_background_opacity = 0.90
 config.window_decorations = "RESIZE"
 config.scrollback_lines = 50000
 config.check_for_updates = false
 config.font = wezterm.font("Victor Mono", { weight = "Medium" })
 config.allow_square_glyphs_to_overflow_width = "Always"
---config.xcursor_theme = "Adwaita"
 
 keybindings.apply_to_config(config)
 --domains.apply_to_config(config)
