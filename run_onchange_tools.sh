2cb6db8047a47bd7a612762fe937c61f52050aa7
d34dsc3n3 <upget_inboards@aleeas.com>
2024-01-19 21:58:07 +0100

    Update tools.sh

--- run_onchange_tools.sh
+++ run_onchange_tools.sh
@@ -86,6 +86,7 @@ if check "go"; then
 	check "shfmt" || go install mvdan.cc/sh/v3/cmd/shfmt@latest
 	check "goimports" || go install golang.org/x/tools/cmd/goimports@latest
 	check "gomodifytags" || go install github.com/fatih/gomodifytags@latest
+	check "payload-dumper-go" || go install github.com/ssut/payload-dumper-go@latest
 	check "wormhole-william" || go install github.com/psanford/wormhole-william@latest
 	check "flarectl" || go install github.com/cloudflare/cloudflare-go/cmd/flarectl@latest
 	check "doggo" || go install github.com/mr-karan/doggo/cmd/doggo@latest
