7458dcf9f5e2281d7ba9e67fd5baf78e9a6b99cd
d34dsc3n3 <upget_inboards@aleeas.com>
2024-04-11 17:53:13 +0200

    Update .config/nvim/lua/configs/conform.lua Update .config/nvim/lua/user/autocmds.lua Update .config/nvim/lua/user/keymaps.lua Update .ssh/config

--- dot_config/nvim/lua/configs/conform.lua
+++ dot_config/nvim/lua/configs/conform.lua
@@ -3,6 +3,7 @@ if not status_ok then
 	return
 end
 
+local slow_format_filetypes = {}
 conform.setup {
 	formatters_by_ft = {
 		lua = { "stylua" },
@@ -23,6 +24,25 @@ conform.setup {
 		["*"] = { "codespell" },
 		["_"] = { "trim_whitespace", "trim_newlines" },
 	},
+	format_on_save = function(bufnr)
+		if slow_format_filetypes[vim.bo[bufnr].filetype] then
+			return
+		end
+		local function on_format(err)
+			if err and err:match "timeout$" then
+				slow_format_filetypes[vim.bo[bufnr].filetype] = true
+			end
+		end
+
+		return { timeout_ms = 200, lsp_fallback = true }, on_format
+	end,
+
+	format_after_save = function(bufnr)
+		if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+			return
+		end
+		return { lsp_fallback = true }
+	end,
 }
 
 -- Add custom args to formatter
