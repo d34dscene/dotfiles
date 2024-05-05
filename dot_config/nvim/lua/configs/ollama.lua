f53e84fd118ef9781a4cc9b96d086825b8d48b9a
d34dsc3n3 <upget_inboards@aleeas.com>
2024-05-05 23:49:47 +0200

    Update .config/nvim/lua/configs/ollama.lua Update .config/nvim/lua/user/keymaps.lua Update .zshrc Update .zshenv

--- dot_config/nvim/lua/configs/ollama.lua
+++ dot_config/nvim/lua/configs/ollama.lua
@@ -8,16 +8,21 @@ local actions = require "ollama.actions.factory"
 
 ollama.setup {
 	url = "http://127.0.0.1:11434",
-	model = "deepseek:latest",
+	model = "llama3:8b",
 	prompts = {
 		Raw = false,
 		Modify_Code = false,
 		Ask_About_Code = false,
+		Ask_About_Buffer = {
+			prompt = "I have a question about this code: \n```$ftype\n$buf\n```\n\n$input",
+			input_label = "Q:",
+			action = actions.create_action { display = true, window = "vsplit" },
+		},
 		Add_Comments = {
-			prompt = "Add useful comments to the following $ftype code without modifying it. Add your comment above the code line. Don't explain every single line, just the important ones. Do not add comments to error handling lines. Each sentence should be put in a separate line. Keep it short and simple!"
+			prompt = "Add documentation to the following $ftype code without modifying it. Adhere to $ftype style docs. Keep it short and simple!"
 				.. response_format
 				.. "\n\n```$ftype\n$sel```",
-			action = "replace",
+			action = actions.create_action { display = true, window = "vsplit", replace = true },
 		},
 		Simplify_Code = {
 			prompt = "Simplify the following $ftype code so that it is both easier to read and understand."
@@ -33,7 +38,7 @@ ollama.setup {
 		},
 		Debug_Buffer = {
 			prompt = "Debug the following $ftype code. Explain the issues and provide the corrected code. Also provide suggestions on how to improve its organization and readability."
-				.. "\n\n```$ftype\n$buf```",
+				.. "\n\n```$ftype\n$buf\n```",
 			action = actions.create_action { display = true, window = "vsplit" },
 		},
 	},
--- dot_config/nvim/lua/user/keymaps.lua
+++ dot_config/nvim/lua/user/keymaps.lua
@@ -13,8 +13,8 @@ local opts = { noremap = true, silent = true }
 map("", "<Space>", "<Nop>")
 
 -- Standard Operations
-map("n", "ss", "<cmd>w<cr>", { desc = "Save" })
-map("n", "qq", "<cmd>q<cr>", { desc = "Quit" })
+map("n", "ss", ":w<cr>", { desc = "Save" })
+map("n", "qq", ":q<cr>", { desc = "Quit" })
 
 -- Search and replace
 map("n", "rt", ":%s///g<Left><Left>", { desc = "Replace all in current buffer" })
@@ -34,21 +34,21 @@ map("n", "<A-h>", "<C-w>h", { desc = "Move to left split" })
 map("n", "<A-j>", "<C-w>j", { desc = "Move to below split" })
 map("n", "<A-k>", "<C-w>k", { desc = "Move to above split" })
 map("n", "<A-l>", "<C-w>l", { desc = "Move to right split" })
-map("n", "<C-k>", "<cmd>resize -2<cr>", { desc = "Resize split up" })
-map("n", "<C-j>", "<cmd>resize +2<cr>", { desc = "Resize split down" })
-map("n", "<C-h>", "<cmd>vertical resize -2<cr>", { desc = "Resize split left" })
-map("n", "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Resize split right" })
+map("n", "<C-k>", ":resize -2<cr>", { desc = "Resize split up" })
+map("n", "<C-j>", ":resize +2<cr>", { desc = "Resize split down" })
+map("n", "<C-h>", ":vertical resize -2<cr>", { desc = "Resize split left" })
+map("n", "<C-l>", ":vertical resize +2<cr>", { desc = "Resize split right" })
 
 -- Navigate buffers
-map("n", "<A-k>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer tab" })
-map("n", "<A-j>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous buffer tab" })
-map("n", "<A-K>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer tab right" })
-map("n", "<A-J>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer tab left" })
-map("n", "mp", "<cmd>BufferLinePick<cr>", { desc = "Pick buffer tab" })
+map("n", "<A-k>", ":BufferLineCycleNext<cr>", { desc = "Next buffer tab" })
+map("n", "<A-j>", ":BufferLineCyclePrev<cr>", { desc = "Previous buffer tab" })
+map("n", "<A-K>", ":BufferLineMoveNext<cr>", { desc = "Move buffer tab right" })
+map("n", "<A-J>", ":BufferLineMovePrev<cr>", { desc = "Move buffer tab left" })
+map("n", "mp", ":BufferLinePick<cr>", { desc = "Pick buffer tab" })
 
 -- Buffer delete/wipeout & quit
-map("n", "qw", "<cmd>w|bp|bd #<cr>", { desc = "Save and close buffer" })
-map("n", "qe", "<cmd>wa|BufferLineCloseOthers<cr>", { desc = "Save and close all buffer" })
+map("n", "qw", ":w|bp|bd #<cr>", { desc = "Save and close buffer" })
+map("n", "qe", ":wa|BufferLineCloseOthers<cr>", { desc = "Save and close all buffer" })
 
 -- Neotree
 map("n", "<leader>e", function()
@@ -60,18 +60,18 @@ map("n", "<leader>e", function()
 end, { desc = "Open Neotree" })
 
 -- Markdown Preview
-map("n", "<leader>m", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Markdown Preview" })
+map("n", "<leader>m", ":MarkdownPreviewToggle<cr>", { desc = "Markdown Preview" })
 
 -- Lazy
-map("n", "<leader>ll", "<cmd>Lazy sync<cr>", { desc = "Update plugins" })
+map("n", "<leader>ll", ":Lazy sync<cr>", { desc = "Update plugins" })
 
 -- LSP
-map("n", "<leader>lm", "<cmd>Mason<cr>", { desc = "Mason UI" })
-map("n", "<leader>lr", "<cmd>LspRestart<cr>", { desc = "Restart LSP" })
+map("n", "<leader>lm", ":Mason<cr>", { desc = "Mason UI" })
+map("n", "<leader>lr", ":LspRestart<cr>", { desc = "Restart LSP" })
 
 -- Formatter
-map("n", "<leader>lf", "<cmd>ConformInfo<cr>", { desc = "Conform Info" })
-map("n", "<leader>li", "<cmd>LspInfo<cr>", { desc = "LSP Info" })
+map("n", "<leader>lf", ":ConformInfo<cr>", { desc = "Conform Info" })
+map("n", "<leader>li", ":LspInfo<cr>", { desc = "LSP Info" })
 
 -- Code actions preview
 map({ "v", "n" }, "<leader>cc", function()
@@ -135,34 +135,29 @@ map("i", "<A-[>", function()
 end, { expr = true })
 
 -- Ollama
-map("n", "<leader>oo", "<cmd><c-u>lua require('ollama').prompt()<cr>", { desc = "Show prompts" })
-map("n", "<leader>og", "<cmd><c-u>lua require('ollama').prompt('Generate_Code')<cr>", { desc = "Generate Code" })
-map("v", "<leader>os", "<cmd><c-u>lua require('ollama').prompt('Simplify_Code')<cr>", { desc = "Simplify Code" })
-map("v", "<leader>op", "<cmd><c-u>lua require('ollama').prompt('Optimize_Code')<cr>", { desc = "Optimize Code" })
-map("v", "<leader>oe", "<cmd><c-u>lua require('ollama').prompt('Explain_Code')<cr>", { desc = "Explain Code" })
-map("v", "<leader>oc", "<cmd><c-u>lua require('ollama').prompt('Add_Comments')<cr>", { desc = "Add Comments" })
+map("n", "<leader>oo", ":<c-u>lua require('ollama').prompt()<cr>", { desc = "Show prompts" })
+map("n", "<leader>og", ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>", { desc = "Generate Code" })
+map("v", "<leader>os", ":<c-u>lua require('ollama').prompt('Simplify_Code')<cr>", { desc = "Simplify Code" })
+map("v", "<leader>op", ":<c-u>lua require('ollama').prompt('Optimize_Code')<cr>", { desc = "Optimize Code" })
+map("v", "<leader>oe", ":<c-u>lua require('ollama').prompt('Explain_Code')<cr>", { desc = "Explain Code" })
+map("v", "<leader>oc", ":<c-u>lua require('ollama').prompt('Add_Comments')<cr>", { desc = "Add Comments" })
 
 -- Comment
 map("n", "x", function()
 	require("Comment.api").toggle.linewise.current(nil, {})
 end, { desc = "Comment line" })
-map(
-	"v",
-	"x",
-	"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-	{ desc = "Comment block" }
-)
+map("v", "x", "<esc>:lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", { desc = "Comment block" })
 
 -- Go
-map("n", "<leader>gf", "<cmd>GoFillStruct<cr>", { desc = "Go fill struct" })
-map("n", "<leader>ga", "<cmd>GoAddTag<cr>", { desc = "Go add tags" })
-map("n", "<leader>gr", "<cmd>GoRmTag<cr>", { desc = "Go remove tags" })
-map("n", "<leader>gm", "<cmd>GoModTidy<cr>", { desc = "Go mod tidy" })
-map("n", "<leader>gl", "<cmd>GoLint<cr>", { desc = "Go lint" })
-map("n", "<leader>gt", "<cmd>GoAddAllTest -bench<cr>", { desc = "Go add tests" })
+map("n", "<leader>gf", ":GoFillStruct<cr>", { desc = "Go fill struct" })
+map("n", "<leader>ga", ":GoAddTag<cr>", { desc = "Go add tags" })
+map("n", "<leader>gr", ":GoRmTag<cr>", { desc = "Go remove tags" })
+map("n", "<leader>gm", ":GoModTidy<cr>", { desc = "Go mod tidy" })
+map("n", "<leader>gl", ":GoLint<cr>", { desc = "Go lint" })
+map("n", "<leader>gt", ":GoAddAllTest -bench<cr>", { desc = "Go add tests" })
 
 -- Git Conflict
-map("n", "<leader>gc", "<cmd>GitConflictListQf<cr>", { desc = "Git conflict" })
+map("n", "<leader>gc", ":GitConflictListQf<cr>", { desc = "Git conflict" })
 
 -- Telescope
 map("n", "<leader>fw", function()
@@ -204,7 +199,7 @@ end, { desc = "Search references" })
 map("n", "<leader>fd", function()
 	require("telescope.builtin").diagnostics()
 end, { desc = "Search diagnostics" })
-map("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Search Todos" })
+map("n", "<leader>ft", ":TodoTelescope<cr>", { desc = "Search Todos" })
 
 -- Flash
 map({ "n", "x", "o" }, "f", function()
@@ -237,7 +232,7 @@ map("n", "+", "<C-a>", opts) -- increment
 map("n", "-", "<C-x>", opts) -- decrement
 map("n", "++", "O<Esc>j", opts) -- newline below
 map("n", "==", "o<Esc>k", opts) -- newline above
-map("n", "--", "<cmd>%&<cr>", opts) -- repeat substitution
+map("n", "--", ":%&<cr>", opts) -- repeat substitution
 map("n", "<C-a>", "gg<S-v>G", opts) -- select all
 map("n", "<C-x>", "^y$jA<Space><Esc>pkdd", opts) -- paste at end of line
 
