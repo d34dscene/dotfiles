2de77f8e8ff12b0116b8fd9c8a1b424ef95fc9f9
d34dscene <d34dscene@pm.me>
2025-06-15 17:15:42 +0200

    Add .config/nvim/lua/configs/catppuccin.lua

---
+++ dot_config/nvim/lua/configs/catppuccin.lua
@@ -0,0 +1,39 @@
+local safe_require = require("utils").safe_require
+local catpuccin = safe_require "catppuccin"
+if not catpuccin then
+	return
+end
+
+catpuccin.setup {
+	flavour = "mocha",
+	transsparent_background = false,
+	integrations = {
+		-- blink_cmp = true,
+		blink_cmp = { style = "bordered" },
+		cmp = true,
+		gitsigns = true,
+		grug_far = true,
+		indent_blankline = { enabled = true, colored_indent_levels = true },
+		mason = true,
+		neotree = true,
+		notify = true,
+		snacks = { enabled = true },
+		telescope = { enabled = true },
+		treesitter = true,
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = { "italic" },
+				hints = { "italic" },
+				warnings = { "italic" },
+				information = { "italic" },
+			},
+			underlines = {
+				errors = { "underline" },
+				hints = { "underline" },
+				warnings = { "underline" },
+				information = { "underline" },
+			},
+		},
+	},
+}
