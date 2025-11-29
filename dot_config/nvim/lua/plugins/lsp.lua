13522b49ea99cf133bfb4d40c06e02100d363980
d34dscene <d34dscene@pm.me>
2025-11-29 19:51:36 +0100

    Update .config/nvim/lua/plugins/lsp.lua

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -46,6 +46,7 @@ local function get_capabilities()
 					didRename = true,
 					willRename = true,
 				},
+				didChangeWatchedFiles = { dynamicRegistration = true },
 			},
 		})
 	else
@@ -211,6 +212,33 @@ local overrides = {
 			"svelte",
 			"vue",
 			"astro",
+		},
+	},
+	gopls = {
+		settings = {
+			gopls = {
+				codelenses = {
+					gc_details = false,
+					generate = true,
+					regenerate_cgo = true,
+					run_govulncheck = true,
+					test = true,
+					tidy = true,
+					upgrade_dependency = true,
+					vendor = true,
+				},
+				analyses = {
+					nilness = true,
+					unusedparams = true,
+					unusedwrite = true,
+					useany = true,
+				},
+				usePlaceholders = true,
+				completeUnimported = true,
+				staticcheck = true,
+				directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules", "-.nvim" },
+				semanticTokens = true,
+			},
 		},
 	},
 	jsonls = {
